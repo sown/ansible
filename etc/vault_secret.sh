58de0ee62e635c4e338bfaced55a122f69d02b8e
Tim Stallard <tim@login>
2024-12-01 15:31:39 +0000

    store vault secret in /root on login servers

--- etc/vault_secret.sh
+++ etc/vault_secret.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-LOCAL_OVERRIDE_FILE=./etc/vault_secret.local.sh
+LOCAL_OVERRIDE_FILE=./etc/vault_secret.local.s
+LOGIN_HELPER_SCRIPT=/usr/local/bin/vault-passphrase-helper
+
+# script on the login servers
+# to read passphrase from /root
+if [ -e "$LOGIN_HELPER_SCRIPT" ]; then
+    exec $LOGIN_HELPER_SCRIPT
+fi
 
 if [[ ! -z "${ANSIBLE_VAULT_PASSWORD}" ]]; then
   echo ${ANSIBLE_VAULT_PASSWORD}
@@ -10,6 +17,6 @@ fi
 if [ -f $LOCAL_OVERRIDE_FILE ]; then
    exec $LOCAL_OVERRIDE_FILE
 else
-    read -p "Please enter the vault password: " pw
+    read -s -p "Please enter the vault password: " pw
     echo $pw
 fi
